68d5a67dc7f23425e33e2e5f8fdcca635630c26c
Mariafrancesca Sacco <masacco@c1r6p1.42firenze.it>
2026-04-04 19:39:58 +0200

    py1

--- Milestone2/Python_01/ex1/ft_garden_data.py
+++ Milestone2/Python_01/ex1/ft_garden_data.py
@@ -7,6 +7,7 @@ class Plant:
     def show(self) -> None:
         print(f"{self.name}: {self.height}cm, {self.age} days old")
 
+
 if __name__ == "__main__":
     print("=== Garden Plant Registry ===")
 
--- Milestone2/Python_01/ex2/ft_plant_growth.py
+++ Milestone2/Python_01/ex2/ft_plant_growth.py
@@ -1,3 +1,34 @@
 class Plant:
-    def __init__(self, name: str, height: int, age: int):
-        
+    def __init__(self, name: str, height: float, age: int) -> None:
+        self.name = name 
+        self.height = height
+        self.days = age
+
+    def grow(self):
+        self.height += 0.8
+
+    def age(self):
+        self.days += 1
+
+def ft_plant_growth():
+    print("=== Garden Plant Growth ===")
+
+    rose = Plant("Rose", 25.0, 30)
+    rose_growth = rose.height
+    print(f"Rose: {round(rose.height, 1)}cm, {rose.days} days old")
+
+    day = 1
+    while day <= 7:
+
+        rose.grow()
+        rose.age()
+
+        print(f"=== Day {day} ===")
+        print(f"Rose: {round(rose.height, 1)}cm, {rose.days} days old")
+    
+        day += 1
+    print("Growth this week: ", round(rose.height - rose_growth, 1), "cm")
+
+    
+if __name__ == "__main__":
+    ft_plant_growth()
