01f0f20abb441333d20e61e38d0c5260032e9410
mafri42 <mariafsacco@gmail.com>
2026-05-23 11:00:16 +0200

    Py3

---
+++ Milestone2/Python_03/ex3/ft_achievement_tracker.py
@@ -0,0 +1,77 @@
+import random
+
+print("=== Achievement Tracker System ===")
+
+achievements = [
+    "Crafting Genius",
+    "World Savior",
+    "Master Explorer",
+    "Collector Supreme",
+    "Untouchable",
+    "Boss Slayer",
+    "Strategist",
+    "Unstoppable",
+    "Speed Runner",
+    "Survivor",
+    "Treasure Hunter",
+    "First Steps",
+    "Sharp Mind",
+    "Hidden Path Finder"
+]
+
+def gen_player_achievements():
+    number = random.randint(5, 9)
+    player_set = set() #insieme vuoto dove metterò gli achievement del giocatore, set non permette duplicati non ha ordine fisso, serve per collezionare elementi unici
+    for _ in range(number): #ripeti N volte, ma NON mi interessa il numero del ciclo
+        achievement = random.choice(achievements)
+        player_set.add(achievement)
+    return player_set #il set di achievement del giocatore
+
+alice = gen_player_achievements()
+bob = gen_player_achievements()
+charlie = gen_player_achievements()
+dylan = gen_player_achievements()
+print("\nPlayer Alice:", alice)
+print("Player Bob:", bob)
+print("Player Charlie:", charlie)
+print("Player Dylan:", dylan)
+
+#tutti quelli che esistono nel gioco
+all_achievements = alice.union(bob, charlie, dylan)
+print("\nAll distinct achievements:", all_achievements)
+
+#solo quelli che hanno tutti i giocatori
+common = alice.intersection(bob, charlie, dylan)
+print("\nCommon achievements:", common)
+
+#SOLO UN GIOCATORE (DIFFERENCE)
+
+#Alice esclusiva (differenza?)
+only_alice = alice.difference(bob.union(charlie, dylan))
+print("\nOnly Alice has:", only_alice)
+
+#Bob esclusivo 
+only_bob = bob.difference(alice.union(charlie, dylan))
+print("Only Bob has:", only_bob)
+
+#charlie esclusivo
+only_charlie = charlie.difference(alice.union(bob, dylan))
+print("Only Charlie has:", only_charlie)
+
+#Dylan esclusivo
+only_dylan = dylan.difference(alice.union(bob, charlie))
+print("Only Dylan has:", only_dylan)
+
+#ACHIEVEMENT MANCANTI
+
+missing_alice = all_achievements.difference(alice)
+print("\nAlice is missing:", missing_alice)
+
+missing_bob = all_achievements.difference(bob)
+print("Bob is missing:", missing_bob)
+
+missing_charlie = all_achievements.difference(charlie)
+print("Charlie is missing:", missing_charlie)
+
+missing_dylan = all_achievements.difference(dylan)
+print("Dylan is missing:", missing_dylan)
---
+++ Milestone2/Python_03/ex4/ft_inventory_system.py
@@ -0,0 +1,46 @@
+import sys
+
+print("=== Inventory System Analysis ===")
+
+args = sys.argv[1:]
+inventory = {}
+
+for param in args: #per ogni elemento dell’input, analizzalo uno per uno
+    if ":" not in param:
+        print(f"Error - invalid parameter '{param}'")
+        continue
+    item, quantity = param.split(":")
+    if item in inventory:
+        print(f"Redundant item '{item}' - discarding")
+        continue
+    try:
+        quantity = int(quantity)
+    except:
+        print(
+            f"Quantity error for '{item}': "
+            f"invalid literal for int() with base 10: '{quantity}'"
+        )
+        continue
+    inventory[item] = quantity
+print("Got inventory:", inventory)
+items = list(inventory.keys())
+print("Item list:", items)
+total = sum(inventory.values())
+print("Total quantity of the", len(inventory), "items:", total)
+for item in inventory:
+    percent = (inventory[item] / total) * 100
+    print("Item", item, "represents", round(percent, 1), "%")
+max_item = min_item = None
+max_qty = -1
+min_qty = float("inf")
+for item, qty in inventory.items():
+    if qty > max_qty:
+        max_qty = qty
+        max_item = item
+    if qty < min_qty:
+        min_qty = qty
+        min_item = item
+print("Item most abundant:", max_item, "with quantity", max_qty)
+print("Item least abundant:", min_item, "with quantity", min_qty)
+inventory["magic_item"] = 1
+print("Updated inventory:", inventory)
