e2980fb4ac7a5ba5dca77b902dfa5898dc56af4f
mafri42 <mariafsacco@gmail.com>
2026-06-14 23:51:03 +0200

    py4

--- Milestone2/python_modules/Python_04/ex3/ft_vault_security.py
+++ Milestone2/python_modules/Python_04/ex3/ft_vault_security.py
@@ -1,6 +1,25 @@
-def secure_archive():
-
+def secure_archive(file_name, action='read', content='') -> tuple[bool, str]:
+    try:
+        if action == 'read':
+            with open(file_name, "r") as file:
+                content = file.read()
+            return (True, content)
+        elif action == 'write':
+            with open(file_name, "w") as file:
+                file.write(content)
+            return (True, 'Content successfully written to file')
+    except Exception as e:
+        return (False, str(e))
 
 
 if __name__ == "__main__":
-    secure_archive()
+    print("=== Cyber Archives Security ===")
+    print("\nUsing 'secure_archive' to read from a nonexistent file:")
+    print(secure_archive('/not/existing/file', 'read'))
+    print("\nUsing 'secure_archive' to read from a inaccessible file:")
+    print(secure_archive('/etc/master.passwd', 'read'))
+    print("\nUsing 'secure_archive' to read from a regular file:")
+    res = secure_archive('text.txt', 'read')
+    print(res)
+    print("\nUsing 'secure_archive' to write previous content to a new file:")
+    print(secure_archive('new.txt', 'write', res[1]))
