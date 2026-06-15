1e5d5ec88464b26ffa173796b839df5ee7832f0f
mafri42 <mariafsacco@gmail.com>
2026-06-15 13:50:45 +0200

    py6

--- Milestone2/python_modules/Python_06/alchemy/__init__.py
+++ Milestone2/python_modules/Python_06/alchemy/__init__.py
@@ -1,1 +1,2 @@
 from .elements import create_air
+from .potions import strenght_potion, healing_potion
---
+++ Milestone2/python_modules/Python_06/alchemy/potions.py
@@ -0,0 +1,15 @@
+from alchemy.elements import create_earth, create_air
+from elements import create_fire, create_water
+
+def healing_potion() -> str:
+    return (
+        f"Healing potion brewed with "
+        f"'{create_earth()}' and '{create_air()}'"
+    )
+
+
+def strenght_potion() -> str:
+    return (
+        f"Strength potion brewed with "
+        f"'{create_fire()}' and '{create_water()}'"
+    )
