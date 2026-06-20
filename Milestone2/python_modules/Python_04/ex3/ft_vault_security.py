19156e0cd15c91c26350713d80dc8b1dae98a8ea
mafri42 <mariafsacco@gmail.com>
2026-06-20 18:36:57 +0200

    py

--- Milestone2/python_modules/Python_04/ex3/ft_vault_security.py
+++ Milestone2/python_modules/Python_04/ex3/ft_vault_security.py
@@ -1,4 +1,8 @@
-def secure_archive(file_name, action='read', content='') -> tuple[bool, str]:
+from typing import Tuple
+
+
+def secure_archive(file_name: str, action: str = 'read',
+                   content: str = '') -> Tuple[bool, str]:
     try:
         if action == 'read':
             with open(file_name, "r") as file:
@@ -8,18 +12,19 @@ def secure_archive(file_name, action='read', content='') -> tuple[bool, str]:
             with open(file_name, "w") as file:
                 file.write(content)
             return (True, 'Content successfully written to file')
-    except Exception as e:
+        return (False, 'Invalid action')
+    except (FileNotFoundError, PermissionError) as e:
         return (False, str(e))
 
 
 if __name__ == "__main__":
     print("=== Cyber Archives Security ===")
     print("\nUsing 'secure_archive' to read from a nonexistent file:")
-    print(secure_archive('/not/existing/file', 'read'))
-    print("\nUsing 'secure_archive' to read from a inaccessible file:")
-    print(secure_archive('/etc/master.passwd', 'read'))
+    print(secure_archive("/not/existing/file"))
+    print("\nUsing 'secure_archive' to read from an inaccessible file:")
+    print(secure_archive("/etc/shadow"))
     print("\nUsing 'secure_archive' to read from a regular file:")
-    res = secure_archive('text.txt', 'read')
+    res = secure_archive("ancient_fragment.txt")
     print(res)
     print("\nUsing 'secure_archive' to write previous content to a new file:")
-    print(secure_archive('new.txt', 'write', res[1]))
+    print(secure_archive("new.txt", "write", res[1]))
