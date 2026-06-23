5bdccced57f22128a1da79b527a5ab5fc1f0c0d1
mafri42 <mariafsacco@gmail.com>
2026-06-23 12:45:11 +0200

    py

--- Milestone2/python_modules/Python_07/battle.py
+++ Milestone2/python_modules/Python_07/battle.py
@@ -1,5 +1,31 @@
-from ex0 import CreatureFactory, FlameFactory, AquaFactory
+from ex0.factory import CreatureFactory, FlameFactory, AquaFactory
+
+
+def testing(factory: CreatureFactory) -> None:
+    print("Testing factory")
+    base = factory.create_base()
+    evolved = factory.create_evolved()
+    print(base.describe())
+    print(base.attack())
+    print(evolved.describe())
+    print(evolved.attack())
+
+
+def fight(flame_factory: FlameFactory, aqua_factory: AquaFactory) -> None:
+    print("Testing battle")
+    flame = flame_factory.create_base()
+    aqua = aqua_factory.create_base()
+    print(flame.describe())
+    print(" vs.")
+    print(aqua.describe())
+    print(" fight!")
+    print(flame.attack())
+    print(aqua.attack())
 
 
 if __name__ == "__main__":
-    
+    testing(FlameFactory())
+    print()
+    testing(AquaFactory())
+    print()
+    fight(FlameFactory(), AquaFactory())
